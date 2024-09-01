2bdfc88f7a3a1787bf1e89226f0f31cfa919722c
SaddasLB <residentangel90@gmail.com>
2024-09-01 04:41:55 +0200

    added keepassxc config / updated dotadd script

--- .config/scripts/dotfiles-add.sh
+++ .config/scripts/dotfiles-add.sh
@@ -5,6 +5,7 @@
 /usr/bin/git --git-dir="$HOME/dotfiles/" --work-tree="$HOME" add ~/.config/colors.css
 /usr/bin/git --git-dir="$HOME/dotfiles/" --work-tree="$HOME" add ~/.config/fish/config.fish
 /usr/bin/git --git-dir="$HOME/dotfiles/" --work-tree="$HOME" add ~/.config/hypr/
+/usr/bin/git --git-dir="$HOME/dotfiles/" --work-tree="$HOME" add ~/.config/keepassxc/keepassxc.ini
 /usr/bin/git --git-dir="$HOME/dotfiles/" --work-tree="$HOME" add ~/.config/kitty/kitty.conf
 /usr/bin/git --git-dir="$HOME/dotfiles/" --work-tree="$HOME" add ~/.config/mako/config
 /usr/bin/git --git-dir="$HOME/dotfiles/" --work-tree="$HOME" add ~/.config/scripts
